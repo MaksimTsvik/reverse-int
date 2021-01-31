818038238b67d8b4f41787b8c4f8637272c8503a
MaksimTsvik <mkdemix@tut.by>
2021-01-31 19:05:48 +0300

    Final commit. Method explanation added.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse(n) {
+    /* 
+    The idea is to convert input number into string -> split it into array -> reverse array -> join back into string
+    And finally parse it into integer (to jetisson '-' sign).
+    */
     return parseInt(n.toString().split('').reverse().join(''));
 }
